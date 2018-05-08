1a8835acc8cb56058d3ef6805e32513fc922fc55
AKolyadin <ant.kolyadin@gmail.com>
2018-05-08 20:21:54 +0300

    Перенес наверх данные

--- index.php
+++ index.php
@@ -3,6 +3,27 @@ $is_auth = (bool) rand(0, 1);
 
 $user_name = 'Константин';
 $user_avatar = 'img/user.jpg';
+ 
+$categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
+
+$ads['1'] = array('name' => '2014 Rossignol District Snowboard', 'ads_category' => $categories[0], 'price' => 10999, 'pic_link' => 'img/lot-1.jpg');
+
+$ads['2'] = array('name' => 'DC Ply Mens 2016/2017 Snowboard', 'ads_category' => $categories[0], 'price' => 159999, 'pic_link' => 'img/lot-2.jpg');
+
+$ads['3'] = array('name' => 'Крепления Union Contact Pro 2015 года размер L/XL', 'ads_category' => $categories[1], 'price' => 8000, 'pic_link' => 'img/lot-3.jpg');
+
+$ads['4'] = array('name' => 'Ботинки для сноуборда DC Mutiny Charocal', 'ads_category' => $categories[2], 'price' => 10999, 'pic_link' => 'img/lot-4.jpg');
+    
+$ads['5'] = array('name' => 'Куртка для сноуборда DC Mutiny Charocal', 'ads_category' => $categories[3], 'price' => 7500, 'pic_link' => 'img/lot-5.jpg');
+
+$ads['6'] = array('name' => 'Маска Oakley Canopy', 'ads_category' => $categories[5], 'price' => 5400, 'pic_link' => 'img/lot-6.jpg');
+
+function f_price_format ($price_value)
+    {
+        $price = number_format(ceil($price_value), 0, ',', ' ') ;
+        $price_value_format = $price .= '<b class="rub">р</b>';
+        return $price_value_format;
+    }
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -49,22 +70,6 @@ $user_avatar = 'img/user.jpg';
 </header>
 
 <main class="container">
-    <?php 
-        $categories = ["Доски и лыжи", "Крепления", "Ботинки", "Одежда", "Инструменты", "Разное"];
-
-        $ads['1'] = array('name' => '2014 Rossignol District Snowboard', 'ads_category' => $categories[0], 'price' => 10999, 'pic_link' => 'img/lot-1.jpg');
-
-        $ads['2'] = array('name' => 'DC Ply Mens 2016/2017 Snowboard', 'ads_category' => $categories[0], 'price' => 159999, 'pic_link' => 'img/lot-2.jpg');
-
-        $ads['3'] = array('name' => 'Крепления Union Contact Pro 2015 года размер L/XL', 'ads_category' => $categories[1], 'price' => 8000, 'pic_link' => 'img/lot-3.jpg');
-
-        $ads['4'] = array('name' => 'Ботинки для сноуборда DC Mutiny Charocal', 'ads_category' => $categories[2], 'price' => 10999, 'pic_link' => 'img/lot-4.jpg');
-            
-        $ads['5'] = array('name' => 'Куртка для сноуборда DC Mutiny Charocal', 'ads_category' => $categories[3], 'price' => 7500, 'pic_link' => 'img/lot-5.jpg');
-        
-        $ads['6'] = array('name' => 'Маска Oakley Canopy', 'ads_category' => $categories[5], 'price' => 5400, 'pic_link' => 'img/lot-6.jpg');
-    ?>
-    
     <section class="promo">
         <h2 class="promo__title">Нужен стафф для катки?</h2>
         <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
@@ -93,13 +98,6 @@ $user_avatar = 'img/user.jpg';
         <div class="lots__header">
             <h2>Открытые лоты</h2>
         </div>
-        <?php function f_price_format ($price_value)
-            {
-                $price = number_format(ceil($price_value), 0, ',', ' ') ;
-                $price_value_format = $price .= '<b class="rub">р</b>';
-                return $price_value_format;
-            }
-        ?>
         <ul class="lots__list">
             <?php foreach ($ads as $key => $lot_value): ?>
                 <li class="lots__item lot">
