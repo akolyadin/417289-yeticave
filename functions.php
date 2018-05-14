1931417771eadf391414c1abb572d0abbc9280df
AKolyadin <ant.kolyadin@gmail.com>
2018-05-14 20:30:08 +0300

    Работа с датами

--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+date_default_timezone_set('Europe/Moscow');
 
 /**
  * Функция преобразования формата цены
@@ -16,7 +17,7 @@ function price_format($price_value) {
  * @return string - сгеннерированный результат
  */
 function render_template($file_name, $data) {
-	$file_path = __DIR__ . "/templates/$file_name.php";
+	$file_path = __DIR__ . "/templates/$file_name.php"; /**уПочему когда убираю кавычки не работает*/
 	if (is_readable($file_path)) {
 		ob_start();
 		extract($data, EXTR_SKIP);
@@ -27,4 +28,17 @@ function render_template($file_name, $data) {
 	else  {
 		return '';
 	}
+}
+
+/**
+ * Функция обратного отсчета времени
+ * @param start_date string - дата начала (по умолчанию сегодня)
+ * @param days integer - количество дней (по умолчанию 1 день)
+ * @return string - счетчик оставшегося времени Ч:М
+ */
+function end_time($start_date = 'now', $days = 1) {
+	$date1 = date('Y-m-d h:i:s', (strtotime($days . 'days')));
+	$date2 = date('Y-m-d 00:00:00', (strtotime($start_date)));
+	/** Не понимаю почему не работает!!! уже наверное все перепробовал*/
+	return date_diff($date1, $date2);
 }
